9ae19378b1f7570d87782c52938178908b08233f
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-13 17:17:31 +0530

    RED : Adds test for get_masked_word

--- test_hangman.py
+++ test_hangman.py
@@ -15,4 +15,24 @@ def test_secret_word_no_punctuation():
 def test_secret_word_no_proper_nouns():
     assert all(hangman.get_secret_word("./test_data/3.words") == "policeman" for _ in range(100))
 
+
+def test_get_masked_word():
+    assert hangman.get_masked_word('elephant', ['x', 'e', 'l', 'q']) == 'ele*****'
+    assert hangman.get_masked_word('elephant', []) == '********'
+    assert hangman.get_masked_word('elephant', ['e', 'l', 'p', 'h', 'a', 'n', 't',]) == 'elephant'
+
+    
+    
+    
+    
+# def test_get_status():
+#     actual_status = hangman.get_status('elephant', ['x','e','l'], 'q', 7)
+#     expected_status = """Secret word : ele*****
+# Guessed letters : e l q x
+# Remaining turns : 6
+#     """
+
+#     assert actual_status == expected_status
+    
+    
     
